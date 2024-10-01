0a50b19df10bc42e3318c56bb1e2276a6d7e096a
gaowen013 <gaowen013@ke.com>
2024-10-01 23:26:03 +0800

    feat: add menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{GlobalWindowEvent, Manager};
+use tauri::{GlobalWindowEvent, Manager, WindowMenuEvent};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -25,8 +25,33 @@ fn handle_window_event(event: GlobalWindowEvent) {
     }
 }
 
+fn handle_menu_event(event: WindowMenuEvent) {
+    match event.menu_item_id() {
+        "quit" => {
+            std::process::exit(0);
+        }
+        "close" => {
+            event.window().close().unwrap();
+        }
+        _ => {}
+    }
+}
+
 fn main() {
+    use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+    // 这里 `"quit".to_string()` 定义菜单项 ID，第二个参数是菜单项标签。
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_submenu(submenu);
+
+
     tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(handle_menu_event)
         .on_window_event(handle_window_event)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
