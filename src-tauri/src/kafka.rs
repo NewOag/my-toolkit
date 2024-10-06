efc9bfbc1653fdb297bb6ce3f8e935f4c0f337dd
gaowen013 <gaowen013@ke.com>
2024-10-06 22:51:49 +0800

    feat: send message command

--- src-tauri/src/kafka.rs
+++ src-tauri/src/kafka.rs
@@ -1,10 +1,21 @@
+use kafka::client::{KafkaClient, ProduceMessage, RequiredAcks};
+use std::time::Duration;
+
 mod message;
 mod connection;
 
-pub fn send_message() {
+const CLIENT: KafkaClient = KafkaClient::new(vec![]);
 
+pub fn init_client(hosts: Vec<String>) {
+    CLIENT = KafkaClient::new(hosts);
 }
 
-pub fn scan_message() {
-
+#[tauri::command]
+pub fn send_message(topic: &str, data: &str) {
+    CLIENT.load_metadata_all().unwrap();
+    let req = vec![ProduceMessage::new(topic, 0, None, Some(data.as_bytes()))];
+    let resp = CLIENT.produce_messages(RequiredAcks::One, Duration::from_millis(100), req);
+    println!("{:?}", resp);
 }
+
+pub fn scan_message() {}
